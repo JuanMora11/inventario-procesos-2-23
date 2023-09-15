e7800db87f36f8b375b02788005136d1abadd7f0
Juan Diego Mora <juan.0909.123@gmail.com>
2023-09-14 20:59:52 -0500

    Capas del modelo User

--- src/main/java/com/procesos/inventario/controller/UserController.java
+++ src/main/java/com/procesos/inventario/controller/UserController.java
@@ -1,15 +1,31 @@
 package com.procesos.inventario.controller;
 
 import com.procesos.inventario.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import com.procesos.inventario.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserService userService;
+    @PostMapping("users")
+    public User create(@RequestBody User user){
+        return userService.createUser(user);
+    }
+
     @GetMapping("users/{id}")
     public User getUserById(@PathVariable Long id){
-        User user = new User(id, "Juan", "Mora", "Esperanza", "Juan.0909.123@gmail.com", "3165484641", "48486548", "106321141");
-        return user;
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("users/{id}")
+    public User update(@RequestBody User user, @PathVariable Long id){
+        return userService.updateUser(user,id);
+    }
+
+    @DeleteMapping("users/{id}")
+    public Boolean delete(@PathVariable Long id){
+        return userService.deleteUserById(id);
     }
 }
